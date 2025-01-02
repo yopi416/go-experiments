dd9cce75d38d47f3ab0b8ac589c58ed1694414f7
Desuyoppi <yoppytennis416@gmail.com>
2025-01-02 12:30:15 +0900

    fix:middleware-overview

--- experiment1/main.go
+++ experiment1/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middleWareOne")
+		next.ServeHTTP(w, r)
+		log.Println("Completed middleWareone")
+	})
+}
+
+func middlewareTwo(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middleWareTwo")
+		next.ServeHTTP(w, r)
+		log.Println("Complted middleWareTwo")
+	})
+}
+
+func final(w http.ResponseWriter, r *http.Request) {
+	log.Println("Executing final")
+	w.Write([]byte("OKOKOK"))
+	log.Println("Complted final")
+}
 
 func main() {
-	fmt.Println("HelloWolrd!!")
+	mux := http.NewServeMux()
+
+	finalHandler := http.HandlerFunc(final)
+	mux.Handle("/", middlewareOne(middlewareTwo(finalHandler)))
+
+	http.ListenAndServe(":8080", mux)
 }
